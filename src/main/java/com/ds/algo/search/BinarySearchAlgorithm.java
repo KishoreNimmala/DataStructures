0e373fc0c7fa43020ee34bbbd3013785f58a23ce
Kishore Nimmala <nkishore779@gmail.com>
2026-06-20 17:38:09 +0000

    Update BinarySearchAlgorithm

--- src/main/java/com/ds/algo/search/BinarySearchAlgorithm.java
+++ src/main/java/com/ds/algo/search/BinarySearchAlgorithm.java
@@ -20,4 +20,24 @@ public class BinarySearchAlgorithm {
 
         return -1; // Target not found
     }
+
+    public static int findTargetPosition(String[] arrs, String target){
+        int left = 0;
+        int right = arrs.length - 1;
+        while(left <= right){
+            int mid = left + (right -left) / 2;
+            
+            int comparison = arrs[mid].compareTo(target);
+            if (comparison == 0) {
+                return mid;
+            } else if (comparison < 0) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+
+        }
+
+        return -1; // Target not found
+    }
 }
